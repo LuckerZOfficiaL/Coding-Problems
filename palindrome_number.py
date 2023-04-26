aa01545b0ffdaed5de7cc8f0305c84ca8858c0bc
Luca Zhou <lucazhou2000@gmail.com>
2023-04-26 18:42:38 +0200

    modify again palindrome_number.py

--- palindrome_number.py
+++ palindrome_number.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 """
 Given an integer x, return true if x is a  palindrome, and false otherwise.
-
 E.g. 121 is palindrome.
+
+I assume that I cannot cast the number into string and directly check the string.
 """
 
-def isPalindrome(n):
-    num = abs(n) # because numbers can also be negative
+def isPalindrome(self, num: int) -> bool:
+    if num < 0: return False
     digit_list = []
     while(True):
         digit = 0
@@ -17,8 +18,7 @@ def isPalindrome(n):
         num = num/10
         if num == 0:
             break
-        
-    for i in range(0, int(len(digit_list)/2)):
+    for i in range(0, math.floor(len(digit_list)/2)):
         if digit_list[i] != digit_list[len(digit_list)-1-i]:
             return False
     return True
@@ -26,3 +26,4 @@ def isPalindrome(n):
         
 print(isPalindrome(122))
 print(isPalindrome(121))
+print(isPalindrome(-121))
